760c86970f91edaadc13eaffc0a4b0c74c90322d
raghavendra8095 <raghavendrataranal@gmail.com>
2024-02-14 12:26:00 +0530

    first commit

---
+++ src/store/post-list-store.jsx
@@ -0,0 +1,82 @@
+import { createContext, useReducer } from "react";
+
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
+
+const postListReducer = (currPostList, action) => {
+  let newPostList = currPostList;
+  if (action.type === "DELETE_POST") {
+    newPostList = currPostList.filter(
+      (post) => post.id !== action.payload.postId
+    );
+  } else if (action.type === "ADD_POST") {
+    newPostList = [action.payload, ...currPostList];
+  }
+  return newPostList;
+};
+
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
+
+  const addPost = (userId, postTitle, postBody, reactions, tags) => {
+    dispatchPostList({
+      type: "ADD_POST",
+      payload: {
+        id: Date.now(),
+        title: postTitle,
+        body: postBody,
+        reactions: reactions,
+        userId: userId,
+        tags: tags,
+      },
+    });
+  };
+
+  const deletePost = (postId) => {
+    dispatchPostList({
+      type: "DELETE_POST",
+      payload: {
+        postId,
+      },
+    });
+  };
+
+  return (
+    <PostList.Provider
+      value={{
+        postList: postList,
+        addPost: addPost,
+        deletePost: deletePost,
+      }}
+    >
+      {children}
+    </PostList.Provider>
+  );
+};
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Going to Gadag",
+    body: "Hi Friends , I am going to gadag for my vacations",
+    reactions: 2,
+    userId: "user-9",
+    tags: ["vaction", "Gadag", "Enjoying"],
+  },
+  {
+    id: "2",
+    title: "pass ho bhai",
+    body: "4 saal ki masthi k baad bhi ho gaye hain pass.Hard to believe it",
+    reactions: 15,
+    userId: "user-12",
+    tags: ["Graduating", "Unbelievable", "Enjoying"],
+  },
+];
+
+export default PostListProvider;
